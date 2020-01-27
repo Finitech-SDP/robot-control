afb4aa5cef73869ece0651d6559d0e0dcaa4032d
Yuanbo <sybtxd1@yahoo.com>
2020-01-27 20:43:50 +0000

    separate package, add a instruction list for a seires instruction

--- main.py
+++ main.py
@@ -1,72 +1,46 @@
 #! /usr/bin/env python3
+
 import re
-
-# from ev3dev2.motor import (
-#     LargeMotor,
-#     OUTPUT_A,
-#     OUTPUT_B,
-#     OUTPUT_C,
-#     OUTPUT_D,
-#     MoveTank,
-#     MoveSteering,
-# )
-import ev3dev.ev3 as ev3
+import basic_movement as move
 
 
-def moveFoward(speed, time):
-    fwdRight.run_timed(speed_sp=speed, time_sp=time)
-    fwdLeft.run_timed(speed_sp=speed, time_sp=time)
-    bwdLeft.run_timed(speed_sp=-speed, time_sp=time)
-    bwdRight.run_timed(speed_sp=-speed, time_sp=time)
-
-
-def moveBackward(speed, time):
-    fwdRight.run_timed(speed_sp=-speed, time_sp=time)
-    fwdLeft.run_timed(speed_sp=-speed, time_sp=time)
-    bwdLeft.run_timed(speed_sp=speed, time_sp=time)
-    bwdRight.run_timed(speed_sp=speed, time_sp=time)
-
-
-def moveLeft():
-    return None
-
-
-def moveRight():
-    return None
-
-
-def moveFowardLeft():
-    return None
-
-
-def moveFowardRight():
-    return None
-
-
-def moveBackwardLeft():
-    return None
-
-
-def moveBackwardRight():
-    return None
-
-
-def rotateAntiClockwise(angle):
-    angle /= 360
-    angle *= 1390
-    fwdLeft.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    fwdRight.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    bwdLeft.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    bwdRight.run_to_rel_pos(position_sp=angle, speed_sp=300)
-
-
-def rotateClockwise(angle):
-    angle /= 360
-    angle *= 1390
-    fwdLeft.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    fwdRight.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    bwdLeft.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    bwdRight.run_to_rel_pos(position_sp=-angle, speed_sp=300)
+def decode(instruction_list):
+    for m in instruction_list:
+        if m.group(6) is None:  # Determine whether it is rotation command
+            if m.group(3) is None:
+                speed = 50  # by default 50% of its rated maximum speed
+            else:
+                speed = int(m.group(3))
+            speed /= 100
+            speed *= 1050
+            if m.group(4) == "":
+                time = 1000  # by default 1000ms
+            else:
+                time = int(m.group(4))
+            direction = m.group(2)
+            if direction == "F":
+                move.moveFoward(speed, time)
+            if direction == "B":
+                move.oveBackward(speed, time)
+            if direction == "R":
+                move.moveRight()
+            if direction == "L":
+                move.moveLeft()
+            if direction == "FR":
+                move.moveFowardRight()
+            if direction == "FL":
+                move.moveFowardLeft()
+            if direction == "BR":
+                move.moveBackwardRight
+            if direction == "BL":
+                move.moveBackwardLeft()
+        else:  # For rotate instruction
+            direction = m.group(6)
+            angle = int(m.group(7))
+            if direction == "RA":
+                move.rotateClockwise(angle)
+            else:
+                move.rotateAntiClockwise(angle)
 
 
 if __name__ == "__main__":
@@ -75,60 +49,25 @@ if __name__ == "__main__":
     [direction speed(percentage of maximum rotate speed) duration(in ms)] or
     [rotate_direction rotate angles]"""
     command_re = "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (\d*))"
-    # fwdLeft = LargeMotor(OUTPUT_B)
-    # fwdRight = LargeMotor(OUTPUT_A)
-    # bwdLeft = LargeMotor(OUTPUT_D)
-    # bwdRight = LargeMotor(OUTPUT_C)
+    begin_list = False
+    instuction_set = []
 
-    fwdLeft = ev3.LargeMotor("outA")  # OutA means the motor connect to EV3 port A.
-    fwdRight = ev3.LargeMotor("outD")
-    bwdLeft = ev3.LargeMotor("outB")
-    bwdRight = ev3.LargeMotor("outC")
-    if not (  # Check whether all the motor are connected.
-        fwdLeft.connected
-        and fwdRight.connected
-        and bwdLeft.connected
-        and bwdRight.connected
-    ):
-        print("motor are not connected")
     while True:
+        if not move.isMotorConnected():
+            print("Motor is not well connected.")
+            break
         command = input(">")  # Fetch input command.
+        if input == "BEGIN":
+            begin_list = True
+        if input == "END":
+            begin_list = False
+            decode(instuction_set)
+            instuction_set.clear()
         m = re.fullmatch(command_re, command)
         if m is None:
             print("Command is not in correct format!")
         else:
-            if m.group(6) is None:  # Determine whether it is rotation command
-                if m.group(3) is None:
-                    speed = 50  # by default 50% of its rated maximum speed
-                else:
-                    speed = int(m.group(3))
-                speed /= 100
-                speed *= 1050
-                if m.group(4) == "":
-                    time = 1000  # by default 1000ms
-                else:
-                    time = int(m.group(4))
-                direction = m.group(2)
-                if direction == "F":
-                    moveFoward(speed, time)
-                if direction == "B":
-                    moveBackward(speed, time)
-                if direction == "R":
-                    moveRight()
-                if direction == "L":
-                    moveLeft()
-                if direction == "FR":
-                    moveFowardRight()
-                if direction == "FL":
-                    moveFowardLeft()
-                if direction == "BR":
-                    moveBackwardRight
-                if direction == "BL":
-                    moveBackwardLeft()
-            else:  # For rotate instruction
-                direction = m.group(6)
-                angle = int(m.group(7))
-                if direction == "RA":
-                    rotateClockwise(angle)
-                else:
-                    rotateAntiClockwise(angle)
+            if begin_list:
+                instuction_set.append(m)
+            else:
+                decode([m])
