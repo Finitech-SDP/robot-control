163606f789f68b4f15361aba467b2ab0f41d8216
Yuanbo <sybtxd1@yahoo.com>
2020-01-27 16:50:55 +0000

    Add instruction

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sdp
+#Introduction
+This is the robot control script written in python.
+Using scp main.py robot@ip_address:/home/robot to send the main script to the EV3 machine, run the script during the console.
+And you can give simple commands to the EV3.
+##Shortcut
+# Direction
+F: Forward
+B: Backward
+L: Left
+R: Right
+FR: FowardRight
+FL: FowardLeft
+BR: BackwardRight
+BL: BackwardLeft
+# Rotation
+RA: Rotate anti clockwise
+RC: Rotate clockwise
+## Basic command format
+###Direction speed time
+Where speed has range 0-100, where 100 means it reaches EV3 motor's maximum rotate speed, 1050 degrees per second.
+Time is in milliseconds.
+###Rotation degree
+Wherer degree should be within 360 degrees.
+## To do
+1. Store a list of command. Execute one by one.
+2. Add a switch function between moving and stopping. i.e switch between run and idle.
+(Add your thinking here!)
--- main.py
+++ main.py
@@ -69,66 +69,66 @@ def rotateClockwise(angle):
     bwdRight.run_to_rel_pos(position_sp=-angle, speed_sp=300)
 
 
-# "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (360|3[0-5][0-9]|[0-2]?[0-9]{1,2}))"
-command_re = "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (\d*))"
-"""Our command's regular expression is
-((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|
-    ((RA|RC) (360|3[0-5][0-9]|[0-2]?[0-9]{1,2}))
-can be seen as [direction speed(percentage) duration(ms)] or
- [rotate_direction rotate angles]"""
-# fwdLeft = LargeMotor(OUTPUT_B)
-# fwdRight = LargeMotor(OUTPUT_A)
-# bwdLeft = LargeMotor(OUTPUT_D)
-# bwdRight = LargeMotor(OUTPUT_C)
-fwdLeft = ev3.LargeMotor("outA")
-fwdRight = ev3.LargeMotor("outD")
-bwdLeft = ev3.LargeMotor("outB")
-bwdRight = ev3.LargeMotor("outC")
-if not (
-    fwdLeft.connected
-    and fwdRight.connected
-    and bwdLeft.connected
-    and bwdRight.connected
-):
-    print("motor are not connected")
-while True:
-    command = input(">")
-    m = re.fullmatch(command_re, command)
-    if m is None:
-        print("Command is not in correct format!")
-    else:
-        if m.group(6) is None:  # Determine whether it is rotation command
-            if m.group(3) is None:
-                speed = 50  # by default 50% of its rated maximum speed
-            else:
-                speed = int(m.group(3))
-            speed /= 100
-            speed *= 1050
-            if m.group(4) == "":
-                time = 1000  # by default 1000ms
-            else:
-                time = int(m.group(4))
-            direction = m.group(2)
-            if direction == "F":
-                moveFoward(speed, time)
-            if direction == "B":
-                moveBackward(speed, time)
-            if direction == "R":
-                moveRight()
-            if direction == "L":
-                moveLeft()
-            if direction == "FR":
-                moveFowardRight()
-            if direction == "FL":
-                moveFowardLeft()
-            if direction == "BR":
-                moveBackwardRight
-            if direction == "BL":
-                moveBackwardLeft()
-        else:  # For rotate instruction
-            direction = m.group(6)
-            angle = int(m.group(7))
-            if direction == "RA":
-                rotateClockwise(angle)
-            else:
-                rotateAntiClockwise(angle)
+if __name__ == "__main__":
+    # "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (360|3[0-5][0-9]|[0-2]?[0-9]{1,2}))"
+    """Our command's format is
+    [direction speed(percentage of maximum rotate speed) duration(in ms)] or
+    [rotate_direction rotate angles]"""
+    command_re = "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (\d*))"
+    # fwdLeft = LargeMotor(OUTPUT_B)
+    # fwdRight = LargeMotor(OUTPUT_A)
+    # bwdLeft = LargeMotor(OUTPUT_D)
+    # bwdRight = LargeMotor(OUTPUT_C)
+
+    fwdLeft = ev3.LargeMotor("outA")  # OutA means the motor connect to EV3 port A.
+    fwdRight = ev3.LargeMotor("outD")
+    bwdLeft = ev3.LargeMotor("outB")
+    bwdRight = ev3.LargeMotor("outC")
+    if not (  # Check whether all the motor are connected.
+        fwdLeft.connected
+        and fwdRight.connected
+        and bwdLeft.connected
+        and bwdRight.connected
+    ):
+        print("motor are not connected")
+    while True:
+        command = input(">")  # Fetch input command.
+        m = re.fullmatch(command_re, command)
+        if m is None:
+            print("Command is not in correct format!")
+        else:
+            if m.group(6) is None:  # Determine whether it is rotation command
+                if m.group(3) is None:
+                    speed = 50  # by default 50% of its rated maximum speed
+                else:
+                    speed = int(m.group(3))
+                speed /= 100
+                speed *= 1050
+                if m.group(4) == "":
+                    time = 1000  # by default 1000ms
+                else:
+                    time = int(m.group(4))
+                direction = m.group(2)
+                if direction == "F":
+                    moveFoward(speed, time)
+                if direction == "B":
+                    moveBackward(speed, time)
+                if direction == "R":
+                    moveRight()
+                if direction == "L":
+                    moveLeft()
+                if direction == "FR":
+                    moveFowardRight()
+                if direction == "FL":
+                    moveFowardLeft()
+                if direction == "BR":
+                    moveBackwardRight
+                if direction == "BL":
+                    moveBackwardLeft()
+            else:  # For rotate instruction
+                direction = m.group(6)
+                angle = int(m.group(7))
+                if direction == "RA":
+                    rotateClockwise(angle)
+                else:
+                    rotateAntiClockwise(angle)
