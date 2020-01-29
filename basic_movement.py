b6f95763e001ea46a89fb4e00a4ded1072822c79
Yuanbo <sybtxd1@yahoo.com>
2020-01-29 13:37:51 +0000

    Bug fix

--- basic_movement.py
+++ basic_movement.py
@@ -1,4 +1,5 @@
 import ev3dev.ev3 as ev3
+import re
 
 # from ev3dev2.motor import (
 #     LargeMotor,
@@ -67,17 +68,60 @@ def moveBackwardRight():
 
 def rotateAntiClockwise(angle):
     angle /= 360
-    angle *= 1390
-    FWDLEFT.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    FWDRIGHT.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    BWDLEFT.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    BWDRIGHT.run_to_rel_pos(position_sp=angle, speed_sp=300)
+    angle *= 1622
+    FWDLEFT.run_to_rel_pos(position_sp=angle, speed_sp=200)
+    FWDRIGHT.run_to_rel_pos(position_sp=-angle, speed_sp=200)
+    BWDLEFT.run_to_rel_pos(position_sp=-angle, speed_sp=200)
+    BWDRIGHT.run_to_rel_pos(position_sp=angle, speed_sp=200)
 
 
 def rotateClockwise(angle):
     angle /= 360
-    angle *= 1390
-    FWDLEFT.run_to_rel_pos(position_sp=-angle, speed_sp=300)
-    FWDRIGHT.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    BWDLEFT.run_to_rel_pos(position_sp=angle, speed_sp=300)
-    BWDRIGHT.run_to_rel_pos(position_sp=-angle, speed_sp=300)
+    angle *= 1622
+    FWDLEFT.run_to_rel_pos(position_sp=-angle, speed_sp=200)
+    FWDRIGHT.run_to_rel_pos(position_sp=angle, speed_sp=200)
+    BWDLEFT.run_to_rel_pos(position_sp=angle, speed_sp=200)
+    BWDRIGHT.run_to_rel_pos(position_sp=-angle, speed_sp=200)
+
+
+def decode(instruction_list):
+    for m in instruction_list:
+        FWDLEFT.wait_until_not_moving()
+        BWDLEFT.wait_until_not_moving()
+        FWDRIGHT.wait_until_not_moving()
+        BWDRIGHT.wait_until_not_moving()
+        if m.group(6) is None:  # Determine whether it is rotation command
+            if m.group(3) is None:
+                speed = 50  # by default 50% of its rated maximum speed
+            else:
+                speed = int(m.group(3))
+            speed /= 100
+            speed *= 1050
+            if m.group(4) == "":
+                time = 1000  # by default 1000ms
+            else:
+                time = int(m.group(4))
+            direction = m.group(2)
+            if direction == "F":
+                moveFoward(speed, time)
+            if direction == "B":
+                moveBackward(speed, time)
+            if direction == "R":
+                moveRight()
+            if direction == "L":
+                moveLeft()
+            if direction == "FR":
+                moveFowardRight()
+            if direction == "FL":
+                moveFowardLeft()
+            if direction == "BR":
+                moveBackwardRight
+            if direction == "BL":
+                moveBackwardLeft()
+        else:  # For rotate instruction
+            direction = m.group(6)
+            angle = int(m.group(7))
+            if direction == "RA":
+                rotateClockwise(angle)
+            else:
+                rotateAntiClockwise(angle)
--- main.py
+++ main.py
@@ -4,45 +4,6 @@ import re
 import basic_movement as move
 
 
-def decode(instruction_list):
-    for m in instruction_list:
-        if m.group(6) is None:  # Determine whether it is rotation command
-            if m.group(3) is None:
-                speed = 50  # by default 50% of its rated maximum speed
-            else:
-                speed = int(m.group(3))
-            speed /= 100
-            speed *= 1050
-            if m.group(4) == "":
-                time = 1000  # by default 1000ms
-            else:
-                time = int(m.group(4))
-            direction = m.group(2)
-            if direction == "F":
-                move.moveFoward(speed, time)
-            if direction == "B":
-                move.oveBackward(speed, time)
-            if direction == "R":
-                move.moveRight()
-            if direction == "L":
-                move.moveLeft()
-            if direction == "FR":
-                move.moveFowardRight()
-            if direction == "FL":
-                move.moveFowardLeft()
-            if direction == "BR":
-                move.moveBackwardRight
-            if direction == "BL":
-                move.moveBackwardLeft()
-        else:  # For rotate instruction
-            direction = m.group(6)
-            angle = int(m.group(7))
-            if direction == "RA":
-                move.rotateClockwise(angle)
-            else:
-                move.rotateAntiClockwise(angle)
-
-
 if __name__ == "__main__":
     # "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (360|3[0-5][0-9]|[0-2]?[0-9]{1,2}))"
     """Our command's format is
@@ -57,12 +18,14 @@ if __name__ == "__main__":
             print("Motor is not well connected.")
             break
         command = input(">")  # Fetch input command.
-        if input == "BEGIN":
+        if command == "BEGIN":
             begin_list = True
-        if input == "END":
+            continue
+        if command == "END":
             begin_list = False
-            decode(instuction_set)
+            move.decode(instuction_set)
             instuction_set.clear()
+            continue
         m = re.fullmatch(command_re, command)
         if m is None:
             print("Command is not in correct format!")
@@ -70,4 +33,4 @@ if __name__ == "__main__":
             if begin_list:
                 instuction_set.append(m)
             else:
-                decode([m])
+                move.decode([m])
