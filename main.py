2efa5050b081238de059ecc7c9c514cb8c1cf009
Yuanbo <sybtxd1@yahoo.com>
2020-01-31 18:39:58 +0000

    now it can receive commands from server

--- main.py
+++ main.py
@@ -1,43 +1,87 @@
 #! /usr/bin/env python3
 import re
+import sys
+import getopt
+import socket
 
 from control import decoder
 from control import movement
+from protocol import protocol
+from time import sleep
 
 
-def main():
-    command_pattern = "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (\d*))"
-    begin_transaction = False
-    instructions = []
+INSTRUCTIONS = []
+BEGIN_TRANSACTION = False
 
+def command_line():
     while True:
         if not movement.is_motor_connected():
             print("Motor is not connected properly")
             break
-
         command = input(">")
+        parse_command(command)
 
-        if command == "STOP":
-            movement.stop()
-            continue
-        if command == "BEGIN":
-            begin_transaction = True
-            continue
-        if command == "END":
-            begin_transaction = False
-            decoder.decode(instructions)
-            instructions.clear()
-            continue
+        
+def parse_command(command):
+    global BEGIN_TRANSACTION
+    global INSTRUCTIONS
 
-        match = re.fullmatch(command_pattern, command)
+    command_pattern = "((FR|FL|BL|BR|[FBRL]) (100|\d?\d)? (\d*))|((RA|RC) (\d*))"
+    if command == "STOP":
+        movement.stop()
+    if command == "BEGIN":
+        BEGIN_TRANSACTION = True
+    if command == "END":
+        BEGIN_TRANSACTION = False
+        decoder.decode(INSTRUCTIONS)
+        INSTRUCTIONS.clear()
 
-        if match is None:
-            print("Incorrect command format")
+    match = re.fullmatch(command_pattern, command)
+
+    if match is None:
+        print("Incorrect command format")
+    else:
+        if BEGIN_TRANSACTION:
+            INSTRUCTIONS.append(match)
         else:
-            if begin_transaction:
-                instructions.append(match)
-            else:
-                decoder.decode([match])
+            decoder.decode([match])
+
+
+# ./main.py server port
+def sock():
+    port = 4444
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(("0.0.0.0", port))
+        s.listen(1)
+        print("waiting for connections")
+        sock = s.accept()[0]
+    finally:
+        s.close()
+
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+    while True:
+        try:
+            msg = protocol.receive_message(sock).decode("ascii")  # "F 100 1000"
+        except BrokenPipeError:
+            break
+
+        while not movement.is_motor_connected():
+            print("Motor is not connected properly")
+            sleep(10)
+
+        parse_command(msg)
+
+
+def main():
+    if len(sys.argv)>1:
+        if sys.argv[1] == 'server':
+            sock()
+    else:
+        command_line()
+    
+    
 
 
 if __name__ == "__main__":
