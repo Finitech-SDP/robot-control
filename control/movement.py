f005d4c0074edb4ee51481f253efe693c969cf20
Yuanbo <sybtxd1@yahoo.com>
2020-02-11 17:34:45 +0000

    rpi

--- control/movement.py
+++ control/movement.py
@@ -1,22 +1,29 @@
 import logging
 import sys
-from time import sleep
+from time import sleep, time
 
-import ev3dev.ev3 as ev3
-from ev3dev2.sensor.lego import GyroSensor
-
+#import ev3dev.ev3 as ev3
+#from ev3dev2.sensor.lego import GyroSensor
+from motors import Motors
 import config
 from util import util
 
-FWDLEFT = ev3.LargeMotor("outA")  # outA means the motor connect to EV3 port A.
-FWDRIGHT = ev3.LargeMotor("outD")
-BWDLEFT = ev3.LargeMotor("outB")
-BWDRIGHT = ev3.LargeMotor("outC")
+#FWDLEFT = ev3.LargeMotor("outA")  # outA means the motor connect to EV3 port A.
+#FWDRIGHT = ev3.LargeMotor("outD")
+#BWDLEFT = ev3.LargeMotor("outB")
+#BWDRIGHT = ev3.LargeMotor("outC")
+'''Motor_id represented'''
+FWDLEFT = 0
+FWDRIGHT = 1
+BWDLEFT = 2
+BWDRIGHT = 3
+mc = Motors()
 
-GYRO = GyroSensor()
-GYRO.mode = "GYRO-ANG"
 
+#GYRO = GyroSensor()
+#GYRO.mode = "GYRO-ANG"
 
+'''
 def stop_when_rotation_complete(angle):
     c_angle = GYRO.angle
     iteration = 0
@@ -37,166 +44,190 @@ def stop_when_rotation_complete(angle):
         continue
 
     stop()
+'''
 
-
-@util.retry(on_fail_message="The motors aren't connected")
+"""@util.retry(on_fail_message="The motors aren't connected")
 def is_motor_connected():
     return (
         FWDLEFT.connected
         and FWDRIGHT.connected
         and BWDLEFT.connected
         and BWDRIGHT.connected
-    )
+    )"""
+
+def stop_when_time_reach(t):
+    curr_time = time()
+    t = float(t)
+    while time()<curr_time+t:
+        sleep(0.01)
+    mc.stop_motors()
 
 
-def exit_if_motors_not_connected():
+  
+"""def exit_if_motors_not_connected():
     if not is_motor_connected():
         logging.critical("Motors are not properly connected, exiting...")
-        sys.exit(1)
+        sys.exit(1)"""
+def stop():
+    mc.stop_motors()
 
-
-def wait_until_stationary():
+"""def wait_until_stationary():
     FWDLEFT.wait_until_not_moving(timeout=1250)
     BWDLEFT.wait_until_not_moving(timeout=1250)
     FWDRIGHT.wait_until_not_moving(timeout=1250)
-    BWDRIGHT.wait_until_not_moving(timeout=1250)
+    BWDRIGHT.wait_until_not_moving(timeout=1250)"""
 
 
 def move_forward(speed, time):
     if time == "":
         time = config.DEFAULT_RUNTIME_MS
-    if time == "-F":
-        FWDLEFT.run_forever(speed_sp=speed)
-        FWDRIGHT.run_forever(speed_sp=speed)
-        BWDLEFT.run_forever(speed_sp=-speed)
-        BWDRIGHT.run_forever(speed_sp=-speed)
+        if time == "-F":
+            mc.move_motor(FWDRIGHT,speed)
+            mc.move_motor(BWDLEFT,-speed) 
     else:
         time = int(time)
-        FWDRIGHT.run_timed(speed_sp=speed, time_sp=time)
-        FWDLEFT.run_timed(speed_sp=speed, time_sp=time)
-        BWDLEFT.run_timed(speed_sp=-speed, time_sp=time)
-        BWDRIGHT.run_timed(speed_sp=-speed, time_sp=time)
-
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed)
+        stop_when_time_reach(time)
 
 def move_backward(speed, time):
     if time == "":
         time = config.DEFAULT_RUNTIME_MS
     if time == "-F":
-        FWDLEFT.run_forever(speed_sp=-speed)
-        FWDRIGHT.run_forever(speed_sp=-speed)
-        BWDLEFT.run_forever(speed_sp=speed)
-        BWDRIGHT.run_forever(speed_sp=speed)
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,+speed) 
     else:
         time = int(time)
-        FWDRIGHT.run_timed(speed_sp=-speed, time_sp=time)
-        FWDLEFT.run_timed(speed_sp=-speed, time_sp=time)
-        BWDLEFT.run_timed(speed_sp=speed, time_sp=time)
-        BWDRIGHT.run_timed(speed_sp=speed, time_sp=time)
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,+speed) 
+        stop_when_time_reach(time)
 
-
-def move_left():
-    return None
-
-
-def move_right():
-    return None
-
-
-def move_forward_left(speed, angle):
-    if angle == "":
-        angle = config.DEFAULT_ANGLE
-    if angle == "-F":
-        FWDLEFT.run_forever(speed_sp=speed)
-        FWDRIGHT.run_forever(speed_sp=speed / 2)
-        BWDLEFT.run_forever(speed_sp=-speed)
-        BWDRIGHT.run_forever(speed_sp=-speed / 2)
+def move_left(speed, time):
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+    if time == "-F":
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,-speed)  
     else:
-        angle = int(angle)
-        FWDLEFT.run_forever(speed_sp=speed)
-        FWDRIGHT.run_forever(speed_sp=speed / 2)
-        BWDLEFT.run_forever(speed_sp=-speed)
-        BWDRIGHT.run_forever(speed_sp=-speed / 2)
+        time = int(time)
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,-speed) 
+        stop_when_time_reach(time)
 
-        stop_when_rotation_complete(angle)
-
-
-def move_forward_right(speed, angle):
-    if angle == "":
-        angle = config.DEFAULT_ANGLE
-    if angle == "-F":
-        FWDLEFT.run_forever(speed_sp=speed / 2)
-        FWDRIGHT.run_forever(speed_sp=speed)
-        BWDLEFT.run_forever(speed_sp=-speed / 2)
-        BWDRIGHT.run_forever(speed_sp=-speed)
-
+def move_right(speed, time):
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+    if time == "-F":
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,speed) 
     else:
-        angle = int(angle)
-        FWDLEFT.run_forever(speed_sp=speed / 2)
-        FWDRIGHT.run_forever(speed_sp=speed)
-        BWDLEFT.run_forever(speed_sp=-speed / 2)
-        BWDRIGHT.run_forever(speed_sp=-speed)
+        time = int(time)
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,speed) 
+        stop_when_time_reach(time)
 
-        stop_when_rotation_complete(angle)
+def move_forward_left(speed, time):
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+    if time == "-F":
+        mc.read_encoder()
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,-speed)
+    else:
+        time = int(time)
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,-speed) 
+        stop_when_time_reach(time)
+
+
+def move_forward_right(speed, time):
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+    if time == "-F":
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,speed)
+    else:
+        time = int(time)
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,speed) 
+        stop_when_time_reach(time)
 
 
 def move_backward_left(speed, angle):
-    if angle == "":
-        angle = config.DEFAULT_ANGLE
-    if angle == "-F":
-        FWDLEFT.run_forever(speed_sp=-speed / 2)
-        FWDRIGHT.run_forever(speed_sp=-speed)
-        BWDLEFT.run_forever(speed_sp=speed / 2)
-        BWDRIGHT.run_forever(speed_sp=speed)
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+    if time == "-F":
+        mc.move_motor(FWDRIGHT,-speed)
+        mc.move_motor(BWDLEFT,speed) 
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,-speed)
     else:
-        angle = int(angle)
-        FWDLEFT.run_forever(speed_sp=-speed / 2)
-        FWDRIGHT.run_forever(speed_sp=-speed)
-        BWDLEFT.run_forever(speed_sp=speed / 2)
-        BWDRIGHT.run_forever(speed_sp=speed)
-
-        stop_when_rotation_complete(angle)
+        time = int(time)
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,speed) 
+        stop_when_time_reach(time)
 
 
 def move_backward_right(speed, angle):
+    if time == "":
+        time = config.DEFAULT_RUNTIME_MS
+        if time == "-F":
+            mc.move_motor(FWDRIGHT,-speed)
+            mc.move_motor(BWDLEFT,speed) 
+            mc.move_motor(FWDLEFT,-speed)
+            mc.move_motor(BWDRIGHT,speed)
+    else:
+        time = int(time)
+        mc.move_motor(FWDRIGHT,speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,speed)
+        mc.move_motor(BWDRIGHT,+speed) 
+        stop_when_time_reach(time)
+
+
+
+def rotate_clockwise(speed):
+    while(mc.read_encoder(FWDRIGHT)<1800):
+        mc.move_motor(FWDRIGHT,-speed)
+        mc.move_motor(BWDLEFT,speed)  
+"""" if angle == "":
+        time = config.DEFAULT_RUNTIME_ANGLE
+        if angle == "-F":
+            mc.move_motor(FWDRIGHT,-speed)
+            mc.move_motor(BWDLEFT,-speed) 
+            mc.move_motor(FWDLEFT,-speed)
+            mc.move_motor(BWDRIGHT,-speed) 
+        else:
+            angle = int(angle)
+            mc.move_motor(FWDRIGHT,-speed)
+            mc.move_motor(BWDLEFT,-speed) 
+            mc.move_motor(FWDLEFT,-speed)
+            mc.move_motor(BWDRIGHT,-speed) 
+            stop_when_time_reach(time) """
+   
+        
+def rotate_anti_clockwise(speed, angle):
     if angle == "":
-        angle = config.DEFAULT_ANGLE
+        time = config.DEFAULT_RUNTIME_ANGLE
     if angle == "-F":
-        FWDLEFT.run_forever(speed_sp=-speed)
-        FWDRIGHT.run_forever(speed_sp=-speed / 2)
-        BWDLEFT.run_forever(speed_sp=speed)
-        BWDRIGHT.run_forever(speed_sp=speed / 2)
+        mc.move_motor(FWDRIGHT,-speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,-speed) 
     else:
         angle = int(angle)
-        FWDLEFT.run_forever(speed_sp=-speed)
-        FWDRIGHT.run_forever(speed_sp=-speed / 2)
-        BWDLEFT.run_forever(speed_sp=speed)
-        BWDRIGHT.run_forever(speed_sp=speed / 2)
-
-        stop_when_rotation_complete(angle)
-
-
-def stop():
-    FWDLEFT.stop(stop_action="brake")
-    FWDRIGHT.stop(stop_action="brake")
-    BWDLEFT.stop(stop_action="brake")
-    BWDRIGHT.stop(stop_action="brake")
-
-
-def rotate_clockwise(speed, angle):
-    FWDLEFT.run_forever(speed_sp=speed)
-    FWDRIGHT.run_forever(speed_sp=-speed)
-    BWDLEFT.run_forever(speed_sp=-speed)
-    BWDRIGHT.run_forever(speed_sp=speed)
-
-    if angle != "-F":
-        stop_when_rotation_complete(angle)
-
-
-def rotate_anti_clockwise(speed, angle):
-    FWDLEFT.run_forever(speed_sp=-speed)
-    FWDRIGHT.run_forever(speed_sp=speed)
-    BWDLEFT.run_forever(speed_sp=speed)
-    BWDRIGHT.run_forever(speed_sp=-speed)
-
-    if angle != "-F":
-        stop_when_rotation_complete(angle)
+        mc.move_motor(FWDRIGHT,-speed)
+        mc.move_motor(BWDLEFT,-speed) 
+        mc.move_motor(FWDLEFT,-speed)
+        mc.move_motor(BWDRIGHT,-speed) 
+        stop_when_time_reach(time)
